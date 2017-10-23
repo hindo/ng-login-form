e04dfbff0302e801b2c3ba6e22aeab5b9e780480
Hindo <piotr.pabich@gmail.com>
2017-10-23 10:20:03 +0200

    App Module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { AuthModule } from './modules/auth/auth.module';
+import { HomeModule } from './modules/home/home.module';
+
+import { FlashMessageService } from './services/flash.service';
+
+import { PageNotFoundComponent } from './components/pagenotfound/pagenotfound.component';
+
+export const ROUTES: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: '/home' },
+  { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PageNotFoundComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(ROUTES),
+    AuthModule,
+    HomeModule,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [ FlashMessageService ],
+  bootstrap: [ AppComponent ]
 })
 export class AppModule { }
